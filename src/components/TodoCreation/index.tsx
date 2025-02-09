db8a0bea411bb6cf8216b515c99465f8e42d7aef
RyutaGoto <kasa8gasa2rs6@gmail.com>
2025-02-10 01:03:41 +0900

    処理の整理とToastの適用

--- src/components/TodoCreation/index.tsx
+++ src/components/TodoCreation/index.tsx
@@ -1,38 +1,81 @@
 import { useDialog } from "@/hooks/useDialog";
 import { Dialog } from "../Dialog";
-import { useTodos } from "@/hooks/useTodos";
-import { FormEvent } from "react";
+import { ChangeEvent, FormEvent, useMemo, useState } from "react";
+import useSWRMutation from "swr/mutation";
+import { useToast } from "@/hooks/useToast";
+import { Toast } from "../Toast";
 
-export const TodoCreation = () => {
-  const { todosIsValidating, mutateTodos } = useTodos();
-  const { ref, showDialog, closeDialog } = useDialog();
-  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    const title = data.get("title") as string;
-
-    await fetch("/api/todos", {
+const postTodo = async (url: string, { arg }: { arg: { title: string } }) => {
+  try {
+    await fetch(url, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ title }),
+      body: JSON.stringify({ title: arg.title }),
     });
-    mutateTodos();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const TodoCreation = () => {
+  const [title, setTitle] = useState("");
+  const [toastMessage, setToastMessage] = useState("");
+  const { dialogRef, showDialog, closeDialog } = useDialog();
+  const { toastRef, showToast } = useToast();
+  const { trigger, isMutating } = useSWRMutation("api/todos", postTodo);
+  const isDisabledClickSubmit = useMemo(
+    () => title === "" || isMutating,
+    [title, isMutating]
+  );
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    try {
+      await trigger({ title });
+      setTitle("");
+      setToastMessage("タスクを作成しました");
+      showToast();
+      closeDialog();
+    } catch (error) {
+      setToastMessage("タスクの作成に失敗しました");
+      showToast();
+    }
   };
+
+  const handleCandel = () => {
+    setTitle("");
+    closeDialog();
+  };
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.target.value);
+  };
+
   return (
     <>
       <button onClick={showDialog}>新規作成</button>
-      <Dialog title="新規作成" ref={ref} onClose={closeDialog}>
+      <Dialog title="新規作成" ref={dialogRef} onClose={closeDialog}>
+        {isMutating && <p>作成中...</p>}
         <form onSubmit={handleSubmit}>
-          <label htmlFor="title">タスク名</label>
-          <input type="text" id="title" name="title" />
-          <button type="button" onClick={closeDialog}>
+          <label htmlFor="title">タイトル</label>
+          <input
+            type="text"
+            id="title"
+            name="title"
+            value={title}
+            onChange={handleChange}
+          />
+          <button type="button" onClick={handleCandel}>
             キャンセル
           </button>
-          <button type="submit">作成</button>
+          <button type="submit" disabled={isDisabledClickSubmit}>
+            作成
+          </button>
         </form>
       </Dialog>
+      <Toast ref={toastRef} message={toastMessage} />
     </>
   );
 };
