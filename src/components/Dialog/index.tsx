18183d34c589d737239ab16d890d1855410e5d65
RyutaGoto <kasa8gasa2rs6@gmail.com>
2025-02-09 22:59:37 +0900

    ダイアログにタイトル用の引数追加

--- src/components/Dialog/index.tsx
+++ src/components/Dialog/index.tsx
@@ -1,16 +1,18 @@
-import { forwardRef, ReactNode, Ref } from "react";
+import { forwardRef, ReactNode } from "react";
 import styles from "./styles.module.scss";
 
 type DialogProps = {
+  title: string;
   onClose: () => void;
   children: ReactNode;
 };
 
 // react19への移行に伴い、forwardRefを廃止して修正する必要がある
 export const Dialog = forwardRef<HTMLDialogElement, DialogProps>(
-  ({ onClose, children }, ref) => {
+  ({ title, onClose, children }, ref) => {
     return (
       <dialog ref={ref} className={styles.dialog} onClick={onClose}>
+        <h2>{title}</h2>
         <button onClick={onClose}>閉じる</button>
         <div onClick={(e) => e.stopPropagation()}>{children}</div>
       </dialog>
