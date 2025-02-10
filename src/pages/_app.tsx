323b0d814450a6e54795c3ed82aad12fb5d85e79
RyutaGoto <kasa8gasa2rs6@gmail.com>
2025-02-10 22:02:28 +0900

    mantineのセットアップ

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { createTheme, MantineProvider } from "@mantine/core";
+import "@mantine/core/styles.css";
+import "@mantine/notifications/styles.css";
+import { Notifications } from "@mantine/notifications";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <MantineProvider>
+      <Notifications />
+      <Component {...pageProps} />
+    </MantineProvider>
+  );
 }
